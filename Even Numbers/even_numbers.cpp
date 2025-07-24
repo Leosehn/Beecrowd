d3efd3d73805d991ffac05cf134aaf0447397f55
leosehn <leonardo.santos007@edu.pucrs.br>
2025-07-24 04:12:13 -0300

    even numbers problem solved

---
+++ Even Numbers/even_numbers.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+ 
+    for (int i = 1; i <= 100; i++) {
+        if (i%2 == 0) {
+            std::cout << i << "\n";
+        }
+    }
+ 
+    return 0;
+}
